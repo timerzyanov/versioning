496812509be18491e0cd12197d680235869a2c7d
ЦИТ РТ-Тимерзянов Динар Рустемович <Dinar.Timerzyanov@tatar.ru>
2022-12-05 12:01:54 +0300

    first commit

---
+++ src/test/java/ru/cit/versioning/VersioningApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cit.versioning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersioningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
